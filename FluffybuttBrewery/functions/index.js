598599a028ac0fb2a4aa3972411ea7d441373e7e
Linda Nordstrom <linda.nordstrom@sky.uk>
2017-05-08 23:21:43 +0100

    remove email details from database when sending email

--- FluffybuttBrewery/functions/index.js
+++ FluffybuttBrewery/functions/index.js
@@ -8,12 +8,15 @@ const mailTransport = nodemailer.createTransport(`smtps://${gmailEmail}:${gmailP
 
 // Listens for new emails added to /emails/:pushId and sends the email to the recipient
 exports.sendEmail = functions.database.ref('/emails/{pushId}').onWrite(event => {
+    if (event.data.val() === null) { return }
+
     let mailOptions = {
         from: event.data.val().sender,
         to: event.data.val().recipients,
         subject: event.data.val().subject,
         text: event.data.val().body
     };
+    event.data.ref.remove();
 
     return mailTransport.sendMail(mailOptions).then(() => {
       console.log('Email sent to:', mailOptions.to);
